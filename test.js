5b43e2e4b30c87bc0f16142816b5a2d2803e3940
mark <markclint302@gmail.com>
2023-05-19 13:22:54 +0800

    modified:   css/style.css 	modified:   index.html 	renamed:    js/main.js -> landing-page.html 	modified:   readme.txt 	modified:   scss/layouts/main.scss 	modified:   signin.html 	new file:   test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+
+
+const text = ["first", "second", "third"];
+
+let blank = "";
+let exe = "eyy!" + "\n";
+
+for (let i = 0; i < text.length; i++) {
+    blank += text[i] + "\n";
+    exe +=  text[i] + "\n";
+
+}
+    console.log(exe);
+   // console.log(blank);
